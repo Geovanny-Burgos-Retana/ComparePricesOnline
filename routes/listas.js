cb929a4d963b889d0360344040cc23c0ce6cf7ba
PotHead <potHead>
2017-11-15 13:40:46 -0600

    Nuevas funciones
    get productos por id + productos de un supermercado + get lista + insertar producto en lista de compras

--- routes/listas.js
+++ routes/listas.js
@@ -41,4 +41,71 @@ router.delete('/lista/:id', function(req, res, next){
     });
 });
 
+//productos por id
+router.get('/listas/:id', function(req, res, next){
+    db.productos.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, producto){
+        if(err){
+            res.send(err);
+        }
+        res.json(producto);
+    });
+});
+
+//productos de un supermercado
+router.get('/listaP/:supermercado', function(req, res, next){
+    db.productos.find({supermercado: req.params.supermercado}, function(err, productos){
+        if(err){
+            res.send(err);
+        }
+        res.json(productos);
+    });
+});
+
+// Get una sola lista 
+router.get('/lista/:id', function(req, res, next){
+    console.log('antes');
+    db.listas.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, task){
+        if(err){
+            console.log('error');
+            res.send(err);
+        }
+        res.json(task);
+    });
+});
+
+// Insertar un producto en lista de compras
+router.put('/listas/:id', function(req, res, next){
+    var lista = req.body;
+    var updLista = {};
+    
+    if(lista.supermercado){
+        updLista.supermercado = lista.supermercado;
+    }
+
+    if(lista.productos){
+        updLista.productos = lista.productos;
+    }
+    
+    if(lista.nombre){
+        updLista.nombre = lista.nombre;
+    }
+    
+    if(!updLista){
+        res.status(400);
+        res.json({
+            "error":"Bad Data"
+        });
+    } else {
+        
+        db.listas.update({_id: mongojs.ObjectId(lista._id)}, {$push: {productos: lista.producto}}, {}, function(err, lista){
+        if(err){
+            
+            res.send(err);
+        }
+        console.log('aqui');
+        res.json(lista);
+    });
+    }
+});
+
 module.exports = router;
